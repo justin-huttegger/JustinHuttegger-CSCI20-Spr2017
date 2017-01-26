56a77333602aeaa1a2afde6a6a23a68d65c3fc1e
Justin Huttegger <jhuttegger001@student.butte.edu>
2017-01-26 23:02:07 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,13 @@
+/*
+    Start program
+    Program randomly selects any number between 0 and 21 and stores the number
+    User guesses the number the program has picked and presses "Enter" key to send to program
+    Program reads input from user and compares it to the number it randomly selected
+    If user input is correct number, they win
+    If user input is too low, program will tell the user to guess "higher" 
+    If user input is too high, program will tell user to guess "lower"
+    Program will let user guess until they get the right number
+    User has 10 points
+    Program keeps score for user
+    Program will subtract 1 point for each incorrect guess
+*/
