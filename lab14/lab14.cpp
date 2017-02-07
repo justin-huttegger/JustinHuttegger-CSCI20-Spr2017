e09b9ef186da7c72ff4c2bcebe71f2ac0d30ef08
Justin Huttegger <jhuttegger001@student.butte.edu>
2017-02-07 23:54:28 +0000

    Lab 1.4 submission

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 int main() {
     int userCents = 0;
-    int outQuarters = .25;
-    int outDimes = .10;
-    int outNickels = .05;
-    int outPennies = .01;
+    int outQuarters = 25;
+    int outDimes = 10;
+    int outNickels = 5;
+    int outPennies = 1;
     
     
     cout << "Enter the amount of cents you have." << endl;
     cin >> userCents;
     
     outQuarters =  userCents / 25;
-    outDimes = userCents % 25;
-    outNickels = userCents % 25;
-    outPennies = userCents % 25;
+    outDimes = userCents % 10;
+    outNickels = userCents % 5;
+    outPennies = userCents % 1;
 
 
-    cout <<"You have " << outQuarters << " quarters, " << outDimes << " dimes, " << outNickels << " nickels, and " << outPennies << " pennies."  << endl; // I DON'T KNOW WHY IT WON'T WORK, QUARTERS WORKS BUT EVERYTHING ELSE DOESN'T
+    cout <<"You have " << outQuarters << " quarters, " << outDimes << " dimes, " << outNickels << " nickels, and " << outPennies << " pennies."  << endl;
     
     return 0;
 }
