626e6d0acffa21851567d3932346f0373114abe1
Justin Huttegger <jhuttegger001@student.butte.edu>
2017-02-21 22:58:49 +0000

    Lab2.1 submission

--- lab16/lab16.cpp
+++ lab16/lab16.cpp
@@ -1,4 +1,8 @@
-//Justin Huttegger
+/*Justin Huttegger
+  CSCI 20
+  Feb 16, 2017
+  1.6 Lab - Objects
+*/
 
 #include <iostream>
 #include <string>
@@ -27,7 +31,45 @@ int main() {
     cout << "Enter a monster name: ";
     cin >> monsterName;
     cout << "Enter the monster characteristics:";
-    cin >> head.MonsterStruct;
+
+    
+    
+    
+    return 0;
+}/*Justin Huttegger
+  CSCI 20
+  Feb 16, 2017
+  1.6 Lab - Objects
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct MonsterStruct { // Struct for all the monster characteristics that will be entered.
+    string monsterZombus = "Zombus";
+    string monsterFranken = "Franken";
+    string monsterHappy = "Happy";
+    string monsterVegitas = "Vegitas";
+    string monsterWackus = "Wackus";
+    string monsterSpritem = "Spritem";
+    string monsterNone = "None";
+} head, eyes, ears, nose, mouth;
+
+    string monsterName = "MonsterOne";
+
+int main() { 
+    
+    cout << monsterName << ": " << head.monsterZombus << ", " << eyes.monsterSpritem << ", " << ears.monsterVegitas << ", " << nose.monsterNone << ", " << mouth.monsterWackus << endl;
+    
+    monsterName = "MonsterTwo";
+    cout << monsterName;
+    cout << ": " << head.monsterFranken << ", " << eyes.monsterHappy << ", " << ears.monsterSpritem << ", " << nose.monsterWackus << ", " << mouth.monsterWackus << endl;
+    
+    cout << "Enter a monster name: ";
+    cin >> monsterName;
+    cout << "Enter the monster characteristics:";
+
     
     
     
---
+++ lab21/lab21.cpp
@@ -0,0 +1,36 @@
+/*Justin Huttegger
+  CSCI 20
+  Feb 16, 2017
+  1.6 Lab - Objects
+  
+  The program outputs information about windchills based on what the user's inputs are.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() { 
+    
+    double OldStyleWindChill = 0.0;
+    double NewStyleWindChill = 0.0;
+    double windVelocity = 0.0;
+    double tempF = 0.0;
+    double speedDifference = 0.0;
+    
+    cout << "Enter the wind speed: " << endl;
+    cin >> windVelocity;
+    
+    cout << "Enter the temperature: " << endl;
+    cin >> tempF;
+    
+    OldStyleWindChill = 0.081 * ( ( 3.71 * (sqrt( windVelocity ) )) +  5.81 - 0.25  * windVelocity ) * ( tempF - 91.4 ) + 91.4;
+    
+    NewStyleWindChill = ( ( 35.74 + 0.6215 ) * tempF ) - ( 35.75 * pow( windVelocity, 0.16) + ( 0.4275 * tempF ) * pow(windVelocity, 0.16 ) );
+    
+    speedDifference = NewStyleWindChill - OldStyleWindChill;
+    
+    cout << "Wind Speed: " << windVelocity << ", Old Formula: " << OldStyleWindChill << ", New Formula: " << NewStyleWindChill << ", Difference: " << speedDifference;
+    
+    return 0;
+}
