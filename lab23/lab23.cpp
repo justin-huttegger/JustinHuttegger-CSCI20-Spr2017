448954fc3676e175f586dff6714fd3ef85bf625d
Justin Huttegger <jhuttegger001@student.butte.edu>
2017-02-28 23:47:49 +0000

    Lab2.3 submission

--- lab23/lab23.cpp
+++ lab23/lab23.cpp
@@ -9,20 +9,21 @@
 #include <ctime>
 using namespace std;
 
-void swapNumber(int &userNumber, int &userNumber2) { //swapping function to change order of numbers
-    userNumber2 = userNumber + userNumber2;
-    userNumber = userNumber2 - userNumber;
-    userNumber2 = userNumber2 - userNumber;
+void Swap(int &userNumber, int &userNumber2) { //swapping function to change order of numbers
+  int x,y;
+  x = userNumber;
+  y = userNumber2;
+  userNumber = y;
+  userNumber2 = x;
     
-    return;
 }
 
  void randomNumber(int &userNumber, int &userNumber2) {
     srand(time(0));
     
     cout << "A random number between ";
-    swapNumber(userNumber, userNumber2);
-    cout << userNumber2 << " and " << userNumber << " is " << rand();
+    Swap(userNumber, userNumber2);
+    cout << userNumber2 << " and " << userNumber << " is " << rand() % userNumber2 + 1;
     
     return;
 } 
@@ -35,7 +36,7 @@ int main()
   cout << "Enter two numbers: ";
   cin >> userNumber >> userNumber2;
  
-  swapNumber(userNumber, userNumber2);
+  Swap(userNumber, userNumber2);
   randomNumber(userNumber, userNumber2);
   
     return 0;
