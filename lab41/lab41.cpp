d6b5f301b4d2eb955009b877ba000e3604095368
Justin Huttegger <jhuttegger001@student.butte.edu>
2017-04-12 03:05:18 +0000

    Lab 4.1 submission

---
+++ lab41/lab41.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include lab41.cpp
+using namespace std;
+
+int main()
+{
+    const int SIZE = 10;
+    int myArray[SIZE];
+    
+    for(int i = 0; i<SIZE;i++){
+        cin<<myArray[i];
+    }
+    ArrayTools a(myArray);
+    
+    a.Print();
+    cout<<"Min: "<<a.Find_min(0, 4)<<endl;
+    cout<<"Max: "<<a.Find_max(5, 10)<<endl;
+    cout<<"Sum = "<<a.Find_sum()<<endl;
+    cout<<"Search 10"<<a.Search(10)<<endl;
+    cout<<"Sorted? "<<a.Is_sorted()<<endl;
+    
+    return 0;
+}
